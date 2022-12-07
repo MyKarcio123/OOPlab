07e55cf6bd39bdcd7f7119eab3bd2ca9da781b25
MyKarcio123 <karol.wieckowiak21@interia.pl>
2022-12-07 19:02:37 +0100

    v1

---
+++ proj1/src/main/java/agh/ics/oop/Vector2d.java
@@ -0,0 +1,75 @@
+package agh.ics.oop;
+
+import java.util.Objects;
+
+public class Vector2d {
+    private final int x;
+    private final int y;
+
+    public Vector2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(%d,%d)".formatted(x, y);
+    }
+
+    public boolean precedes(Vector2d other) {
+        return x <= other.x && y <= other.y;
+    }
+
+    public boolean follows(Vector2d other) {
+        return x >= other.x && y >= other.y;
+    }
+
+    public Vector2d add(Vector2d other) {
+        return new Vector2d(x + other.x, y + other.y);
+    }
+
+    public Vector2d substract(Vector2d other) {
+        return new Vector2d(x - other.x, y - other.y);
+    }
+
+    public Vector2d upperRight(Vector2d other) {
+        return new Vector2d(Math.max(x, other.x), Math.max(y, other.y));
+    }
+
+    public Vector2d lowerLeft(Vector2d other) {
+        return new Vector2d(Math.min(x, other.x), Math.min(y, other.y));
+    }
+
+    public Vector2d opposite() {
+        return new Vector2d(-x, -y);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof Vector2d vector2dOther) {
+            return (x == vector2dOther.x && y == vector2dOther.y);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    public static void main(String[] args) {
+        Vector2d position1 = new Vector2d(1, 2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2, 1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
