f40d6d7680d0886cf33d74f10217b58d5f966d2d
puszekjuliuszek <tojabonibykto@gmail.com>
2022-12-09 17:06:02 +0100

    map v1

---
+++ proj1/src/main/java/agh/ics/oop/IWorldMap.java
@@ -0,0 +1,12 @@
+package agh.ics.oop;
+
+public interface IWorldMap {
+
+    boolean canMoveTo(Vector2d position);
+
+    boolean place(Animal animal);
+
+    boolean isOccupied(Vector2d position);
+
+    Object objectAt(Vector2d position);
+}
