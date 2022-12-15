4e65e0907fdfb4bd19d5eeb24343024101bd6abb
puszekjuliuszek <tojabonibykto@gmail.com>
2022-12-15 20:07:33 +0100

    s

---
+++ proj1/src/main/java/agh/ics/oop/gui/GuiElementBox.java
@@ -0,0 +1,32 @@
+package agh.ics.oop.gui;
+
+import agh.ics.oop.AbstractMapElement;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+public class GuiElementBox {
+
+    private Image image;
+    private ImageView imageView = new ImageView();
+    private VBox vBox = new VBox();
+
+    public GuiElementBox(AbstractMapElement element) {
+            this.image = new Image("file:src/main/resources/" + element.getImagePath() + ".png");
+            imageView.setImage(image);
+            imageView.setFitHeight(20);
+            imageView.setFitWidth(20);
+
+
+
+        Label label = new Label(element.getDesc());
+        vBox.getChildren().addAll(imageView, label);
+        vBox.setAlignment(Pos.CENTER);
+    }
+
+    public VBox getvBox() {
+        return vBox;
+    }
+}
