f7a850621dc393802ce9e8b2531bf72c356e4554
Karol <43847587+MyKarcio123@users.noreply.github.com>
2022-12-16 00:44:33 +0100

    simapp

--- proj1/src/main/java/agh/ics/oop/gui/MainMenuController.java
+++ proj1/src/main/java/agh/ics/oop/gui/MainMenuController.java
@@ -1,38 +1,42 @@
 package agh.ics.oop.gui;
 
-import com.google.common.io.Files;
-import com.google.errorprone.annotations.FormatMethod;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.control.Slider;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.RadioButton;
 import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
 
+import javafx.scene.control.TextField;
 import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class MainMenuController {
+public class MainMenuController implements IMainMenuControllerObserver{
+
+    private List<TextField> textFields = new ArrayList<>();
+    private List<RadioButton> radioButtonList = new ArrayList<>();
     private List<ConfigButton> configButtons = new ArrayList<>();
     private List<String> currentConfig = new ArrayList<>();
+    @Override
+    public void setCurrentConfig(List<String> currentConfig) {
+        this.currentConfig = new ArrayList<>(currentConfig);
+        updateData();
+    }
+
     @FXML
     private GridPane settingGrid;
 
     @FXML
     private void makeButton(List<String> configProperties) {
-        ConfigButton currentButton = new ConfigButton(configProperties);
+        ConfigButton currentButton = new ConfigButton(configProperties,(IMainMenuControllerObserver) this);
         configButtons.add(currentButton);
         settingGrid.add(currentButton.getPane(), 0, settingGrid.getRowCount());
     }
 
     @FXML
-    public void loadSaves() {
+        public void loadSaves() {
         URL resource = getClass().getResource("/saves.txt");
         if (resource == null) throw new IllegalArgumentException("File not found!");
         FileReader fr = null;
@@ -54,6 +58,7 @@ public class MainMenuController {
                     configProperties.clear();
                 }
             }
+            reader.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -77,17 +82,36 @@ public class MainMenuController {
 
     @FXML
     protected void onSaveFileButtonClick() {
+        currentConfig = loadData();
         FileChooser fc = new FileChooser();
         fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
 
         File file = fc.showSaveDialog(null);
+
         if (file != null) {
             saveTextToFile(file);
+        }
+    }
+    @FXML
+    protected void onAddToFavoritesButtonClick(){
+        try{
+            currentConfig = new ArrayList<>(loadData());
+            BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(Objects.requireNonNull(getClass().getResource("/template.txt")).getPath())));
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(String.valueOf(Objects.requireNonNull(getClass().getResource("/saves.txt")).getPath()),true)));
+            for (String s : currentConfig) {
+                out.println(reader.readLine() + s);
+            }
+            makeButton(currentConfig);
+            reader.close();
+            out.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
     private void saveTextToFile(File file) {
         try {
+            currentConfig = new ArrayList<>(loadData());
             PrintWriter writer;
             writer = new PrintWriter(file);
 
@@ -110,6 +134,82 @@ public class MainMenuController {
             writer.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+    @FXML
+    TextField configurationName;
+    @FXML
+    TextField heighField;
+    @FXML
+    TextField widthField;
+    @FXML
+    RadioButton earthVariant;
+    @FXML
+    TextField grassAmountField;
+    @FXML
+    TextField energyFromGrassField;
+    @FXML
+    TextField grassPerDayField;
+    @FXML
+    RadioButton goodVariant;
+    @FXML
+    TextField startingAnimalField;
+    @FXML
+    TextField startingEnergyField;
+    @FXML
+    TextField energyNeededField;
+    @FXML
+    TextField energyCopulationField;
+    @FXML
+    TextField minMutationField;
+    @FXML
+    TextField maxMutationField;
+    @FXML
+    RadioButton randomVariant;
+    @FXML
+    TextField genomeLengthField;
+    @FXML
+    RadioButton fullControllVariant;
+    private void loadToArray(){
+        textFields.clear();
+        textFields.add(configurationName);
+        textFields.add(heighField);
+        textFields.add(widthField);
+        radioButtonList.add(earthVariant);
+        textFields.add(grassAmountField);
+        textFields.add(energyFromGrassField);
+        textFields.add(grassPerDayField);
+        radioButtonList.add(goodVariant);
+        textFields.add(startingAnimalField);
+        textFields.add(startingEnergyField);
+        textFields.add(energyNeededField);
+        textFields.add(energyCopulationField);
+        textFields.add(minMutationField);
+        textFields.add(maxMutationField);
+        radioButtonList.add(randomVariant);
+        radioButtonList.add(fullControllVariant);
+        textFields.add(genomeLengthField);
+    }
+    private List<String> loadData(){
+        loadToArray();
+        currentConfig.clear();
+        for (TextField textField : textFields) {
+            currentConfig.add(textField.toString());
+        }
+        for (RadioButton radioButton : radioButtonList) {
+            if (radioButton.isSelected()) currentConfig.add("0");
+            else currentConfig.add("1");
+        }
+        return currentConfig;
+    }
+    private void updateData(){
+        loadToArray();
+        for(int i=0;i<currentConfig.size();++i){
+            if(i<=12){
+                textFields.get(i).setText(currentConfig.get(i));
+            }else{
+                radioButtonList.get(i-12).setSelected(Objects.equals(currentConfig.get(i), "0"));
+            }
         }
     }
 }
