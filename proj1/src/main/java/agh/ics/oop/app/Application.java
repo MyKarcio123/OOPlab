013561361dae8c8c11162abc22cc9c52f7f81b26
puszekjuliuszek <tojabonibykto@gmail.com>
2022-12-15 17:32:44 +0100

    gui start

---
+++ proj1/src/main/java/agh/ics/oop/app/Application.java
@@ -0,0 +1,11 @@
+package agh.ics.oop.app;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class Application {
+
+    @FXML
+    private GridPane ;
+}
