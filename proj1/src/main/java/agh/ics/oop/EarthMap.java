c009e3637c3e108d1c3fd33fa4a435c31fd927b2
puszekjuliuszek <tojabonibykto@gmail.com>
2022-12-25 21:36:59 +0100

    canMoveTo

--- proj1/src/main/java/agh/ics/oop/EarthMap.java
+++ proj1/src/main/java/agh/ics/oop/EarthMap.java
@@ -1,5 +1,8 @@
 package agh.ics.oop;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 public class EarthMap extends AbstractWorldMap{
     public EarthMap(IMapStateEngineObserver observer, DataParameters currentConfig) {
         super(observer, currentConfig);
@@ -9,17 +12,21 @@ public class EarthMap extends AbstractWorldMap{
     public boolean canMoveTo(Vector2d position) {
 
         int y = position.getY();
+        int x = position.getX();
 
-        return y>this.getMapLowerLeft().getY() && y<this.getMapUpperRight().getY();
+        return y>=this.getMapLowerLeft().getY() && y<=this.getMapUpperRight().getY() && x>=this.getMapLowerLeft().getX() && x<=this.getMapUpperRight().getX();
     }
 
     @Override
     public Vector2d getNewPosition(Vector2d position) {
+        int y = position.getY();
+        y = min(y, getMapUpperRight().getY());
+        y = max(y, getMapLowerLeft().getY());
         if (position.getX() > getMapUpperRight().getX()){
-            return new Vector2d(getMapLowerLeft().getX(),position.getY());
+            return new Vector2d(getMapLowerLeft().getX(), y);
         }
         if (position.getX()< getMapLowerLeft().getX()){
-            return new Vector2d(getMapUpperRight().getX(),position.getY());
+            return new Vector2d(getMapUpperRight().getX(),y);
         }
         return null;
     }
