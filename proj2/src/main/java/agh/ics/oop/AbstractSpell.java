71e86525a7692a6b35651d65c80ec5340ef6ccb7
puszekjuliuszek <tojabonibykto@gmail.com>
2023-01-18 03:07:52 +0100

    player1

---
+++ proj2/src/main/java/agh/ics/oop/AbstractSpell.java
@@ -0,0 +1,11 @@
+package agh.ics.oop;
+
+public class AbstractSpell {
+    private int healthDamage;
+    private int manaDamage;
+
+    public AbstractSpell(int healthDamage, int manaDamage){
+        this.healthDamage = healthDamage;
+        this.manaDamage = manaDamage;
+    }
+}
